64abb712c4e03c2849091eda5ee8530b21fc0c49
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2026-09-22 02:15:05 +0900

    docs: 공통 엔티티 역할 설명 추가

--- src/main/java/com/bareum/server/global/entity/BaseTimeEntity.java
+++ src/main/java/com/bareum/server/global/entity/BaseTimeEntity.java
@@ -10,6 +10,9 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+/**
+ * 엔티티의 생성 시간과 마지막 수정 시간을 자동으로 기록하는 공통 부모 클래스
+ */
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
