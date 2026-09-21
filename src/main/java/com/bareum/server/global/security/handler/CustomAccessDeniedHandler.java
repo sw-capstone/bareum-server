ca310bc27c9d938a00c41f856fc81bfe36666731
Kwon-DoHee <152317074+seamooll@users.noreply.github.com>
2026-09-22 02:14:51 +0900

    chore: 인증 및 인가 예외 처리 클래스 추가

---
+++ src/main/java/com/bareum/server/global/security/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,6 @@
+package com.bareum.server.global.security.handler;
+
+public class CustomAccessDeniedHandler {
+
+	// TODO: Spring Security 도입 시 ProblemDetailFactory로 권한 부족 응답 생성
+}
---
+++ src/main/java/com/bareum/server/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,6 @@
+package com.bareum.server.global.security.handler;
+
+public class CustomAuthenticationEntryPoint {
+
+	// TODO: Spring Security 도입 시 ProblemDetailFactory로 인증 실패 응답 생성
+}
